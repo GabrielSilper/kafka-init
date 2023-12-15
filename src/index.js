f594dfe7b9af5e4822c257ddaef1af6f5f71a275
GabrielSilper <gabrielsilper.main@gmail.com>
2023-12-15 11:19:30 -0400

    feat: criado um producer e um consumer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const { sendMessage } = require('./kafka-poducer-config');
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.send({ ok: true, status: 'Server running' });
+});
+
+app.post('/message', async (req, res) => {
+  const { message } = req.body;
+  await sendMessage(message);
+  res.send({ ok: true });
 });
 
 app.listen(3000, () => {
